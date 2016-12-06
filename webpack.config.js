d8cab3b274f48c7e06f9408a0b20b1190793b7f9
Eun Min Kim <midi_lall@midis-iMac.local>
2016-12-06 17:53:39 -0500

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+module.exports = {
+  entry: [
+    './src/index.js'
+  ],
+  output: {
+    path: __dirname,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    preLoaders: [
+      {
+          test: /\.js$/, // include .js files
+          exclude: /node_modules/, // exclude any and all files in the node_modules folder
+          loader: "jshint-loader"
+      }
+    ],
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['react', 'es2015', 'stage-1']
+        }
+      },
+      {
+        test: /\.scss$/, loader:'style!css!sass'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './',
+    port:1333
+  }
+};
